2721a90ce6f4d3754e7e06390be5af0c0267e3ad
Saikiran7777 <saikiraneshwer@gmail.com>
2020-02-28 18:55:49 -0600

    added windows

--- script.sh
+++ script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#This will give all numbers from 1 to 100
-for i in {1..100}
-do
-if [ $(expr $i % 2) -eq 0 ]
-then
-echo "$i is a even number"
-else
-echo "$i is a odd number except it is zero"
-fi
+#This will give all numbers from 1 to 100 by windows
+for i in {1..100}; do
+    if [ $(expr $i % 2) -eq 0 ]; then
+        echo "$i is a even number"
+    else
+        echo "$i is a odd number except it is zero"
+    fi
 done
 #End of the script......!by sai
